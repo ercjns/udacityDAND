435afe2289b31eb258a76c578c6d221bfa2ab9c9
ercjns <eric@ercjns.com>
2017-08-26 11:40:52 -0700

    regression lesson

--- ud120-MachineLearning/regression/finance_regression.py
+++ ud120-MachineLearning/regression/finance_regression.py
@@ -22,6 +22,7 @@ dictionary = pickle.load( open("../final_project/final_project_dataset_modified.
 ### list the features you want to look at--first item in the 
 ### list will be the "target" feature
 features_list = ["bonus", "salary"]
+# features_list = ['bonus', 'long_term_incentive']
 data = featureFormat( dictionary, features_list, remove_any_zeroes=True)
 target, features = targetFeatureSplit( data )
 
@@ -29,7 +30,7 @@ target, features = targetFeatureSplit( data )
 from sklearn.cross_validation import train_test_split
 feature_train, feature_test, target_train, target_test = train_test_split(features, target, test_size=0.5, random_state=42)
 train_color = "b"
-test_color = "b"
+test_color = "r"
 
 
 
@@ -39,10 +40,14 @@ test_color = "b"
 ### "r" to differentiate training points from test points.
 
 
+from sklearn.linear_model import LinearRegression
+reg = LinearRegression()
+reg.fit(feature_train, target_train)
+print 'Slope: {}'.format(reg.coef_)
+print 'Intercept: {}'.format(reg.intercept_)
 
-
-
-
+print 'Score (TRAIN): {}'.format(reg.score(feature_train, target_train))
+print 'Score (TEST): {}'.format(reg.score(feature_test, target_test))
 
 
 ### draw the scatterplot, with color-coded training and testing points
@@ -64,7 +69,17 @@ try:
     plt.plot( feature_test, reg.predict(feature_test) )
 except NameError:
     pass
+
+### added lines to fit test data
+reg.fit(feature_test, target_test)
+plt.plot(feature_train, reg.predict(feature_train), color="r") 
+###
+
 plt.xlabel(features_list[1])
 plt.ylabel(features_list[0])
 plt.legend()
 plt.show()
+
+### display test data fit params:
+print 'Test Data Slope: {}'.format(reg.coef_)
+print 'Test Data Intercept: {}'.format(reg.intercept_)
