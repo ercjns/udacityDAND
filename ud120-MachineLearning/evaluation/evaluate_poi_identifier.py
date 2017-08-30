7a6ed70b0eb68f5d22c23921eadfa888e15a6db4
ercjns <eric@ercjns.com>
2017-08-30 10:59:32 -0700

    evaluation lesson

--- ud120-MachineLearning/evaluation/evaluate_poi_identifier.py
+++ ud120-MachineLearning/evaluation/evaluate_poi_identifier.py
@@ -27,5 +27,55 @@ labels, features = targetFeatureSplit(data)
 
 
 ### your code goes here 
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.cross_validation import train_test_split
+
+features_train, features_test, labels_train, labels_test = train_test_split(features, labels, test_size=0.3, random_state=42)
+
+clf = DecisionTreeClassifier()
+clf.fit(features_train, labels_train)
+print clf.score(features_test, labels_test)
+
+# How many POI in test set?
+print sum(labels_test) # 4
+
+# How many people in test set?
+print len(labels_test) # 29
+
+# What's accuracy for predicting all non-POI
+# 25/29 = .86
+
+# are there any true positives?
+pred = clf.predict(features_test)
+pairs = zip(pred, labels_test)
+tp = 0
+for x in pairs:
+    if x == (1.0, 1.0):
+        tp += 1
+print tp #0
+
+from sklearn.metrics import precision_score, recall_score
+print 'precision: {}'.format(precision_score(labels_test, pred)) #0
+print 'recall: {}'.format(recall_score(labels_test, pred)) #0
+
+### Working with some fake data:
+predictions = [0, 1, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 1, 0, 1] 
+truth = [0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 0]
+
+paired = zip(predictions, truth)
+
+true_positives = sum([1 for (p,t) in paired if (p,t)==(1,1)])
+true_negatives = sum([1 for (p,t) in paired if (p,t)==(0,0)])
+false_positives = sum([1 for (p,t) in paired if (p,t)==(1,0)])
+false_negatives = sum([1 for (p,t) in paired if (p,t)==(0,1)])
+print 'true positives: {}'.format(true_positives)
+print 'true negatives: {}'.format(true_negatives)
+print 'false positives: {}'.format(false_positives)
+print 'false negatives: {}'.format(false_negatives)
+
+precision = true_positives / float(true_positives + false_positives)
+recall = true_positives / float(true_positives + false_negatives)
+print 'precision: {}'.format(precision)
+print 'recall: {}'.format(recall)
 
 
